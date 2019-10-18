84f2a56ba8f87a4a26dc81b7df2fa71a3a0e6e7b
Oluwajoba <oluwajobabello@gmail.com>
2019-10-18 08:47:37 +0100

    use token to navigate to the BubblePage route / Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,7 +22,7 @@ const Login = (props) => {
       .then(res => {
         // debugger
         localStorage.setItem("token", res.data.payload)
-       
+        props.history.push("/bubblePage");
       });
   };
   return (
